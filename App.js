80feef636f4c67337b08ad0dc25a7b76994fb573
ncir jaweher <60673984+jaweherncir@users.noreply.github.com>
2022-12-18 22:25:43 +0100

    How to Create React Native

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Alert, Pressable } from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+    const [phoneNumber, setPhoneNumber] = React.useState('');
+    const phoneInput = React.useRef(null);
+
+    const OnPress = () => {
+        if (phoneNumber.length !==0) {
+            Alert.alert(
+                "Confirm Number",
+                phoneNumber,
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                    },
+
+                    { 
+                        text: "OK", 
+                        onPress: () => console.log("OK Pressed"),
+                    },
+                ],
+            );
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <PhoneInput
+                ref={phoneInput}
+                defaultValue={phoneNumber}
+                containerStyle={styles.phoneContainer}
+                textContainerStyle={styles.textInput}
+                onChangeFormattedText={text => {
+                    setPhoneNumber(text);
+                }}
+                defaultCode="IN"
+                layout='first'
+                withShadow
+                autoFocus
+            />
+            <Pressable
+                style={styles.button}
+                onPress={() => OnPress()}
+                android_ripple="red"
+            >
+                <Text style={styles.text}>Send OTP</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    phoneContainer: {
+        width: '75%',
+        height: 50,
+    },
+    button: {
+        marginTop: 30,
+        width: '75%',
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#18d4d4',
+    },
+    textInput: {
+        paddingVertical: 0,
+    },
+    text: {
+        color:'white',
+        fontWeight:'600'
+    },
 });
+
+export default App;
